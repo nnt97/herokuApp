b13716d3b314ee2e90f13e125cd75863b41b44e4
Thai Nguyen <thainn2@vng.com.vn>
2019-07-03 22:36:21 +0700

    d

--- src/main/java/com/example/demo/controller/RestController.java
+++ src/main/java/com/example/demo/controller/RestController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
 
-	@RequestMapping(value="/webhook/trigger", method=RequestMethod.POST)
+//	@RequestMapping(value="/webhook/trigger", method=RequestMethod.POST)
+	@PostMapping("/webhook/trigger")
 	public String show(@RequestBody Map<String,String> body) {
 		System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 		return "thainn2";
